eab172a94bd0e38996c7a77c9313ce9c93d2f728
Samuel Snopko <samuel@snopko.com>
2020-04-14 14:33:33 +0200

    updated welcome screen

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <div>
       <logo />
       <h1 class="title">
-        talk-blazingly-fast-nuxtjs
+        Going headless with Nuxt!
       </h1>
       <h2 class="subtitle">
-        My great Nuxt.js project
+        How to connect Nuxt.js with Storyblok
       </h2>
       <div class="links">
         <a
