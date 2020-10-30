051c6bfd9ed1973f275a9cd57ebac19f1e125c7c
Samuel Snopko <samuel@snopko.com>
2020-10-30 14:57:05 +0100

    fixed welcome screen

--- pages/index.vue
+++ pages/index.vue
@@ -36,6 +36,8 @@
 </template>
 
 <script>
+import Logo from "~/components/Logo";
+
 export default {
   components: {
     Logo
@@ -87,3 +89,49 @@ export default {
   // }
 }
 </script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
