b632eaa539826c0007ad9a87fa577227ac1ea6af
Samuel Snopko <samuel@snopko.com>
2021-05-20 10:50:33 +0200

    refactor: updated to storyblok-nuxt v2 & update of packages

--- pages/index.vue
+++ pages/index.vue
@@ -48,23 +48,26 @@ export default {
   //   }
   // },
   // mounted () {
-  //   // // Use the input event for instant update of content
-  //   // this.$storybridge.on('input', (event) => {
-  //   //   if (event.story.id === this.story.id) {
-  //   //     this.$storybridge.resolveRelations(['featured-articles.articles'], (event) => {
-  //   //       // event.story.content has now the resolved relations
-  //   //       this.story.content = event.story.content
-  //   //     })
-  //   //   }
+  //   // this.$storybridge(() => {
+  //   //   const storyblokInstance = new StoryblokBridge({
+  //   //     resolveRelations: ['featured-articles.articles']
+  //   //   })
+  //   //   // Listen to Storyblok Visual Editor events
+  //   //   storyblokInstance.on(['input', 'published', 'change'], (event) => {
+  //   //     if (event.action == 'input') {
+  //   //       if (event.story.id === this.story.id) {
+  //   //         this.story.content = event.story.content
+  //   //       }
+  //   //     } else {
+  //   //       this.$nuxt.$router.go({
+  //   //         path: this.$nuxt.$router.currentRoute,
+  //   //         force: true,
+  //   //       })
+  //   //     }
+  //   //   })
+  //   // }, (error) => {
+  //   //   console.error(error)
   //   // })
-  //   // Use the bridge to listen the events
-  //   this.$storybridge.on(['published', 'change'], (event) => {
-  //     // window.location.reload()
-  //     this.$nuxt.$router.go({
-  //       path: this.$nuxt.$router.currentRoute,
-  //       force: true,
-  //     })
-  //   })
   // },
   // asyncData (context) {
   //   // // This what would we do in real project
