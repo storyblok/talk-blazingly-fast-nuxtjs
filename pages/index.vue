d4691a9679153cf1a342dccad905c9d6b3d64080
Samuel Snopko <samuel@snopko.com>
2021-05-20 10:47:46 +0200

    refactor: updated to storyblok-nuxt v2 & updated rest of packages

--- pages/index.vue
+++ pages/index.vue
@@ -16,22 +16,25 @@ export default {
     }
   },
   mounted () {
-    // Use the input event for instant update of content
-    this.$storybridge.on('input', (event) => {
-      if (event.story.id === this.story.id) {
-        this.$storybridge.resolveRelations(['featured-articles.articles'], (event) => {
-          // event.story.content has now the resolved relations
-          this.story.content = event.story.content
-        })
-      }
-    })
-    // Use the bridge to listen the events
-    this.$storybridge.on(['published', 'change'], (event) => {
-      // window.location.reload()
-      this.$nuxt.$router.go({
-        path: this.$nuxt.$router.currentRoute,
-        force: true,
+    this.$storybridge(() => {
+      const storyblokInstance = new StoryblokBridge({
+        resolveRelations: ['featured-articles.articles']
       })
+      // Listen to Storyblok Visual Editor events
+      storyblokInstance.on(['input', 'published', 'change'], (event) => {
+        if (event.action == 'input') {
+          if (event.story.id === this.story.id) {
+            this.story.content = event.story.content
+          }
+        } else {
+          this.$nuxt.$router.go({
+            path: this.$nuxt.$router.currentRoute,
+            force: true,
+          })
+        }
+      })
+    }, (error) => {
+      console.error(error)
     })
   },
   asyncData (context) {
